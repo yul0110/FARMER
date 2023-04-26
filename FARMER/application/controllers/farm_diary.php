1364ed8272d506ca77856e935a3f5d33a63e7ae3
Lee Yul <gpal0110naver.com>
2023-04-26 14:58:31 +0900

    id 넘버링 설정

--- FARMER/application/controllers/farm_diary.php
+++ FARMER/application/controllers/farm_diary.php
@@ -12,10 +12,11 @@ class farm_diary extends CI_Controller {
 	
 	//일지등록 ajax
 	public function diary()
-	{
+	{		
 		// model('diarymodel')로드시킴
 		$this->load->model('diarymodel'); 
-		$result = $this->diarymodel->insert_diary();  
+		$number_result = $this->diarymodel->numbering_diary();  
+		$result = $this->diarymodel->insert_diary($number_result);  
 		
 		//데이터 result
 		echo json_encode(array(
--- FARMER/application/models/diarymodel.php
+++ FARMER/application/models/diarymodel.php
@@ -9,15 +9,32 @@ class diarymodel extends CI_Model {
         parent::__construct();
     }
 
-    function insert_diary()
-    {
+    function numbering_diary(){
+        
+        $this->load->database();
+
+        $this->db->select('id');
+        $this->db->from('farmDiary');
+        $this->db->order_by("id", "desc");
+        $this->db->limit(1,0);
+        $query=$this->db->get();
+
+        //결과 값을 배열로($row) 가져오기 때문에 foreach로 값을 꺼내줌
+        foreach ($query->result() as $row){
+            $numbering = $row->id;
+        }
+
+        return $numbering;
+    }
+
+    function insert_diary($number_result){
         $this->load->database();
 
         //한국시간으로 디폴트 설정
         date_default_timezone_set("Asia/Seoul");
 
         $data = array(
-            'id'          => 1,
+            'id'          => $number_result + 1,
             'title'       => $this->input->post('title'),
             'contents'    => $this->input->post('contents'),
             'imgGroupId'  => '123',
@@ -33,6 +50,5 @@ class diarymodel extends CI_Model {
         $update_flag = $this->db->insert('farmDiary', $data);
 
         return $update_flag;
-
     }
 }
