246b7d47bdbbbebef41e116e75c7c4ef1c20c8de
Lee Yul <gpal0110naver.com>
2023-05-04 00:25:21 +0900

    멤버 테이블 분리 설정

--- FARMER/application/config/authit.php
+++ FARMER/application/config/authit.php
@@ -8,7 +8,7 @@
  * @version 1.0
  */
 
-$config['authit_users_table'] = 'users';
+$config['authit_users_table'] = 'member';
 $config['authit_password_min_length'] = 4;
 // Do you want to test the emails you send out? 보낸 메일을 테스트하시겠습니까?
 // If you do you can view the results by going to ../auth/sentemails  원할 경우 ../auth/sentemails
